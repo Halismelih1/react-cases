7758535f8b672fcb4246412faee22f8cc45841a9
Halismelih1 <hhlsabkk@icloud.com>
2023-12-25 13:38:20 +0300

    Project created

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import { useGetProductsQuery } from '../Redux/api';
+import Card from './Card';
+
+
+
+const Hero = () => {
+
+    return(
+        <Card/>
+    )
+    
+}
+
+export default Hero;
+
+
+
