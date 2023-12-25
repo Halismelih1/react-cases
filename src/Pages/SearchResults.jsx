b724e562391aca26523474a9f95449106c992705
Halismelih1 <hhlsabkk@icloud.com>
2023-12-25 17:12:18 +0300

    search issues fixed

--- src/Pages/SearchResults.jsx
+++ src/Pages/SearchResults.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Card from '../Components/Card';
+import { useLocation } from 'react-router-dom';
+
 
 const SearchResults = ({ data }) => {
   const [searchResults, setSearchResults] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     const searchQuery = new URLSearchParams(window.location.search).get('q');
@@ -17,7 +20,7 @@ const SearchResults = ({ data }) => {
 
       setSearchResults(filteredResults);
     }
-  }, [data]);
+  }, [data,location]);
 
   return (
     <div className="container mx-auto flex flex-wrap">
