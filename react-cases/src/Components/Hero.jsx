2586ae314a232e42303641a3f06dc9fec5d90980
Halismelih1 <hhlsabkk@icloud.com>
2023-12-26 15:31:35 +0300

    add new folder

--- react-cases/src/Components/Hero.jsx
+++ react-cases/src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import Card from '../Components/Card';
+import Card from './Card';
 
 const Hero = ({ data }) => {
   const [products, setProducts] = useState([]);
